be7bb3b56f252576a66dcb6fd2d2a1c2a8856086
Paul Heasley <paul@phdesign.com.au>
2020-12-12 10:16:24 +1100

    Working on day4 part2

--- day4/day4.go
+++ day4/day4.go
@@ -1,80 +1,137 @@
 package main
 
 import (
-    "strings"
-    //"fmt"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"os"
+	"strconv"
 )
 
 type Passport struct {
-    birthYear string
-    issueYear string
-    expiryYear string
-    height string
-    hairColor string
-    eyeColor string
-    passportId string
-    countryId string
+	birthYear  string
+	issueYear  string
+	expiryYear string
+	height     string
+	hairColor  string
+	eyeColor   string
+	passportId string
+	countryId  string
 }
 
-func (p Passport) isValid() bool {
-    if p.birthYear == "" {
-        return false
-    }
-    if p.issueYear == "" {
-        return false
-    }
-    if p.expiryYear == "" {
-        return false
-    }
-    if p.height == "" {
-        return false
-    }
-    if p.hairColor == "" {
-        return false
-    }
-    if p.eyeColor == "" {
-        return false
-    }
-    if p.passportId == "" {
-        return false
-    }
-    return true
+func (p Passport) isValid(validator Validator) bool {
+	return validator(p)
+}
+
+type Validator func(Passport) bool
+
+func RequiredFieldValidator(p Passport) bool {
+	if p.birthYear == "" {
+		return false
+	}
+	if p.issueYear == "" {
+		return false
+	}
+	if p.expiryYear == "" {
+		return false
+	}
+	if p.height == "" {
+		return false
+	}
+	if p.hairColor == "" {
+		return false
+	}
+	if p.eyeColor == "" {
+		return false
+	}
+	if p.passportId == "" {
+		return false
+	}
+	return true
+}
+
+func FieldValueValidator(p Passport) bool {
+	if !RequiredFieldValidator(p) {
+		return false
+	}
+	if len(p.birthYear) != 4 {
+		return false
+	}
+	if v, _ := strconv.Atoi(p.birthYear); v < 1920 || v > 2002 {
+		return false
+	}
+	if len(p.issueYear) != 4 {
+		return false
+	}
+	if v, _ := strconv.Atoi(p.issueYear); v < 2010 || v > 2020 {
+		return false
+	}
+	if len(p.expiryYear) != 4 {
+		return false
+	}
+	if v, _ := strconv.Atoi(p.expiryYear); v < 2020 || v > 2030 {
+		return false
+	}
+
+	if len(p.passportId) != 9 {
+		return false
+	}
+	if _, err := strconv.Atoi(p.passportId); err != nil {
+		return false
+	}
+	return true
 }
 
 func ParseFields(input string) map[string]string {
-    fields := strings.Fields(input)
-    m := make(map[string]string)
-    for _, field := range fields {
-        parts := strings.Split(field, ":")
-        m[parts[0]] = parts[1]
-    }
-    return m
+	fields := strings.Fields(input)
+	m := make(map[string]string)
+	for _, field := range fields {
+		parts := strings.Split(field, ":")
+		m[parts[0]] = parts[1]
+	}
+	return m
 }
 
 func ParsePassports(input string) []Passport {
-    paragraphs := strings.Split(input, "\n\n")
-    passports := make([]Passport, len(paragraphs))
-    for i, paragraph := range paragraphs {
-        fields := ParseFields(paragraph)
-        passports[i] = Passport{
-            birthYear: fields["byr"],
-            issueYear: fields["iyr"],
-            expiryYear: fields["eyr"],
-            height: fields["hgt"],
-            hairColor: fields["hcl"],
-            eyeColor: fields["ecl"],
-            passportId: fields["pid"],
-            countryId: fields["cid"],
-        }
-    }
-    return passports
+	paragraphs := strings.Split(input, "\n\n")
+	passports := make([]Passport, len(paragraphs))
+	for i, paragraph := range paragraphs {
+		fields := ParseFields(paragraph)
+		passports[i] = Passport{
+			birthYear:  fields["byr"],
+			issueYear:  fields["iyr"],
+			expiryYear: fields["eyr"],
+			height:     fields["hgt"],
+			hairColor:  fields["hcl"],
+			eyeColor:   fields["ecl"],
+			passportId: fields["pid"],
+			countryId:  fields["cid"],
+		}
+	}
+	return passports
 }
 
-func ValidatePasswords(passports []Passport) (count int) {
-    for _, passport := range passports {
-        if passport.isValid() {
-            count++
-        }
-    }
-    return
+func ValidatePassports(passports []Passport, validator Validator) (count int) {
+	for _, passport := range passports {
+		if passport.isValid(validator) {
+			count++
+		}
+	}
+	return
+}
+
+func main() {
+	flag.Parse()
+	filename := flag.Arg(0)
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	input := strings.Trim(string(content), "\n")
+	passports := ParsePassports(input)
+	result := ValidatePassports(passports, RequiredFieldValidator)
+	fmt.Println(result)
 }
--- day4/day4_test.go
+++ day4/day4_test.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-    "reflect"
-    "testing"
+	"reflect"
+	"testing"
 )
 
 func TestParseFields(t *testing.T) {
-    actual := ParseFields("ecl:gry pid:860033327")
-    expected := map[string]string{
-        "ecl": "gry",
-        "pid": "860033327",
-    }
+	got := ParseFields("ecl:gry pid:860033327")
+	want := map[string]string{
+		"ecl": "gry",
+		"pid": "860033327",
+	}
 
-    if !reflect.DeepEqual(actual, expected) {
-        t.Errorf("Expected %v, received %v", expected, actual)
-    }
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Expected %v, received %v", want, got)
+	}
 }
 
 func TestParsePassports(t *testing.T) {
-    input := `ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+	input := `ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
 byr:1937 iyr:2017 cid:147 hgt:183cm
 
 iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
@@ -32,32 +32,104 @@ hgt:179cm
 hcl:#cfa07d eyr:2025 pid:166559648
 iyr:2011 ecl:brn hgt:59in`
 
-    actual := ParsePassports(input)
-    expected := []Passport{
-        Passport{birthYear: "1937", issueYear: "2017", expiryYear: "2020", height: "183cm", hairColor: "#fffffd", eyeColor: "gry", passportId: "860033327", countryId: "147"},
-        Passport{birthYear: "1929", issueYear: "2013", expiryYear: "2023", height: "", hairColor: "#cfa07d", eyeColor: "amb", passportId: "028048884", countryId: "350"},
-        Passport{birthYear: "1931", issueYear: "2013", expiryYear: "2024", height: "179cm", hairColor: "#ae17e1", eyeColor: "brn", passportId: "760753108", countryId: ""},
-        Passport{birthYear: "", issueYear: "2011", expiryYear: "2025", height: "59in", hairColor: "#cfa07d", eyeColor: "brn", passportId: "166559648", countryId: ""},
-    }
+	got := ParsePassports(input)
+	want := []Passport{
+		{birthYear: "1937", issueYear: "2017", expiryYear: "2020", height: "183cm", hairColor: "#fffffd", eyeColor: "gry", passportId: "860033327", countryId: "147"},
+		{birthYear: "1929", issueYear: "2013", expiryYear: "2023", height: "", hairColor: "#cfa07d", eyeColor: "amb", passportId: "028048884", countryId: "350"},
+		{birthYear: "1931", issueYear: "2013", expiryYear: "2024", height: "179cm", hairColor: "#ae17e1", eyeColor: "brn", passportId: "760753108", countryId: ""},
+		{birthYear: "", issueYear: "2011", expiryYear: "2025", height: "59in", hairColor: "#cfa07d", eyeColor: "brn", passportId: "166559648", countryId: ""},
+	}
 
-    if !reflect.DeepEqual(actual, expected) {
-        t.Errorf("Expected %v, received %v", expected, actual)
-    }
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Expected %v, received %v", want, got)
+	}
 }
 
 func TestPassports(t *testing.T) {
-    passport := Passport{birthYear: "1937", issueYear: "2017", expiryYear: "2020", height: "183cm", hairColor: "#fffffd", eyeColor: "gry", passportId: "860033327", countryId: "147"}
-    actual := passport.isValid()
-    assertTrue(t, actual)
+	t.Run("should return true when all required fields are set given required fields validator", func(t *testing.T) {
+		passports := []Passport {
+			Passport{birthYear: "1937", issueYear: "2017", expiryYear: "2020", height: "183cm", hairColor: "#fffffd", eyeColor: "gry", passportId: "860033327", countryId: "147"},
+		}
+		for _, passport := range passports {
+			t.Run("", func(t *testing.T) {
+				got := passport.isValid(RequiredFieldValidator)
+				assertTrue(t, got)
+			})
+		}
+	})
+
+	t.Run("should return false when required fields are missing given required fields validator", func(t *testing.T) {
+		passports := []Passport {
+			Passport{birthYear: "1929", issueYear: "2013", expiryYear: "2023", height: "", hairColor: "#cfa07d", eyeColor: "amb", passportId: "028048884", countryId: "350"},
+		}
+		for _, passport := range passports {
+			t.Run("", func(t *testing.T) {
+				got := passport.isValid(RequiredFieldValidator)
+				assertFalse(t, got)
+			})
+		}
+	})
+
+	t.Run("should return true when all fields are valid given field values validator", func(t *testing.T) {
+		passports := []Passport {
+			Passport{birthYear: "2002", issueYear: "2017", expiryYear: "2020", height: "183cm", hairColor: "#fffffd", eyeColor: "gry", passportId: "860033327", countryId: "147"},
+			Passport{birthYear: "2002", issueYear: "2017", expiryYear: "2020", height: "183cm", hairColor: "#fffffd", eyeColor: "gry", passportId: "060033327", countryId: "147"},
+		}
+		for _, passport := range passports {
+			t.Run("", func(t *testing.T) {
+				got := passport.isValid(FieldValueValidator)
+				assertTrue(t, got)
+			})
+		}
+	})
+
+	t.Run("should return false when any fields are invalid given field values validator", func(t *testing.T) {
+		passports := []Passport {
+			Passport{birthYear: "2003", issueYear: "2017", expiryYear: "2020", height: "183cm", hairColor: "#fffffd", eyeColor: "gry", passportId: "860033327", countryId: "147"},
+			Passport{birthYear: "2002", issueYear: "2017", expiryYear: "2020", height: "183cm", hairColor: "#fffffd", eyeColor: "gry", passportId: "06003332", countryId: "147"},
+			Passport{birthYear: "2002", issueYear: "2017", expiryYear: "2020", height: "183cm", hairColor: "#fffffd", eyeColor: "gry", passportId: "86003332a", countryId: "147"},
+		}
+		for _, passport := range passports {
+			t.Run("", func(t *testing.T) {
+				got := passport.isValid(FieldValueValidator)
+				assertFalse(t, got)
+			})
+		}
+	})
 }
 
 func TestValidatePassports(t *testing.T) {
+	passports := []Passport{
+		{birthYear: "1937", issueYear: "2017", expiryYear: "2020", height: "183cm", hairColor: "#fffffd", eyeColor: "gry", passportId: "860033327", countryId: "147"},
+		{birthYear: "1929", issueYear: "2013", expiryYear: "2023", height: "", hairColor: "#cfa07d", eyeColor: "amb", passportId: "028048884", countryId: "350"},
+		{birthYear: "1931", issueYear: "2013", expiryYear: "2024", height: "179cm", hairColor: "#ae17e1", eyeColor: "brn", passportId: "760753108", countryId: ""},
+		{birthYear: "", issueYear: "2011", expiryYear: "2025", height: "59in", hairColor: "#cfa07d", eyeColor: "brn", passportId: "166559648", countryId: ""},
+	}
+	want := 2
+	got := ValidatePassports(passports, RequiredFieldValidator)
 
+	if got != want {
+		t.Errorf("Expected %d, received %d", want, got)
+	}
 }
 
-func assertTrue(t *testing.T, actual bool) {
-    t.Helper()
-    if !actual {
-        t.Error("Expected result to be true, was false")
-    }
+func assertTrue(t *testing.T, got bool) {
+	t.Helper()
+	if !got {
+		t.Error("Expected result to be true, was false")
+	}
+}
+
+func assertFalse(t *testing.T, got bool) {
+	t.Helper()
+	if got {
+		t.Error("Expected result to be false, was true")
+	}
+}
+
+func assertBool(t *testing.T, got, want bool) {
+	t.Helper()
+	if got != want {
+		t.Errorf("Expected result to be %v, was %v", got, want)
+	}
 }
