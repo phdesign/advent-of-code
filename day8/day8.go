1c9a7a29c5141c69dfecc11da72882a236ae6dc8
Paul Heasley <paul.heasley@acloud.guru>
2020-12-17 17:03:19 +1100

    Complete tests for day8

--- day8/day8.go
+++ day8/day8.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 //type Context struct {
 //accumulator int
 //pc          int
@@ -22,30 +28,61 @@ package main
 //return &Accumulate{argument: argument}
 //}
 
+type InfiniteLoopError struct {
+	instruction int
+}
+
+func (e *InfiniteLoopError) Error() string {
+	return fmt.Sprintf("Infinite loop detected at instruction %d!", e.instruction)
+}
+
 type Instruction struct {
 	operation string
 	argument  int
 }
 
-func RunProgram(input string) int {
-	instructions := Parse(input)
+func Parse(input string) (instructions []Instruction) {
+	expressions := strings.Split(input, "\n")
+	for _, expression := range expressions {
+		tokens := strings.Fields(expression)
+		if len(tokens) != 2 {
+			panic(fmt.Sprintf("Unable to parse expression %q", expression))
+		}
+		operation := tokens[0]
+		argument, err := strconv.Atoi(tokens[1])
+		if err != nil {
+			panic(err)
+		}
+		instructions = append(instructions, Instruction{operation, argument})
+	}
+	return
+}
+
+func Evaluate(instructions []Instruction) (int, error) {
 	pc := 0
 	acc := 0
 	call_stack := make([]int, 0)
 
-	for {
+	for pc < len(instructions) {
+		// Check for infinite loops
+		for _, prev := range call_stack {
+			if prev == pc {
+				return acc, &InfiniteLoopError{pc}
+			}
+		}
+		call_stack = append(call_stack, pc)
 		instruction := instructions[pc]
-		pc++
 		switch instruction.operation {
 		case "nop":
-			break
+			pc++
 		case "acc":
 			acc += instruction.argument
+			pc++
 		case "jmp":
 			pc += instruction.argument
 		}
 
 	}
 
-	return 0
+	return acc, nil
 }
--- day8/day8_test.go
+++ day8/day8_test.go
@@ -1,8 +1,11 @@
 package main
 
-import "testing"
+import (
+	//"reflect"
+	"testing"
+)
 
-func TestRunProgram(t *testing.T) {
+func TestParseAndEvaluate(t *testing.T) {
 	input := `nop +0
 acc +1
 jmp +4
@@ -12,10 +15,40 @@ acc -99
 acc +1
 jmp -4
 acc +6`
-	got := RunProgram(input)
+	instructions := Parse(input)
+	got, err := Evaluate(instructions)
 	want := 5
 
+	//assertError(t, err, reflect.TypeOf(InfiniteLoopError{}))
+	if _, ok := err.(*InfiniteLoopError); !ok {
+		t.Error("Wanted InfiniteLoopError, got none")
+	}
 	assertIntEqual(t, got, want)
+}
+
+func TestEvaluate(t *testing.T) {
+	instructions := []Instruction{
+		Instruction{"nop", 0},
+	}
+	got, err := Evaluate(instructions)
+	want := 0
+
+	assertNoError(t, err)
+	assertIntEqual(t, got, want)
+}
+
+//func assertError(t *testing.T, err error, errorType reflect.Type) {
+//t.Helper()
+//if _, ok := err.(*errorType); !ok {
+//t.Errorf("Wanted %v error type, got %v", errorType, err)
+//}
+//}
+
+func assertNoError(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Errorf("Wanted no error, got %v", err)
+	}
 }
 
 func assertIntEqual(t *testing.T, got, want int) {
