a3ef393ddfd2947e07eb992a764886c7a08ff637
Paul Heasley <paul@phdesign.com.au>
2020-12-20 13:08:21 +1100

    WIP day10 part2

--- day10/day10.go
+++ day10/day10.go
@@ -33,6 +33,10 @@ func Sort(items []int) []int {
 	return sorted
 }
 
+func MultiplyEnds(items []int) int {
+	return items[0] * items[len(items)-1]
+}
+
 func FindJoltDifferences(sorted []int) []int {
 	diffCount := make([]int, 3)
 	for i, item := range sorted {
@@ -50,38 +54,7 @@ func FindJoltDifferences(sorted []int) []int {
 	return diffCount
 }
 
-func Factorial(num int) int {
-	f := 1
-	for i := 1; i <= num; i++ {
-		f = f * i
-	}
-	return f
-}
-
-func Combinations(n int, r int) int {
-	return Factorial(n) / (Factorial(r) * Factorial(n-r))
-}
-
-func Mask(items []int, mask int) (result []int) {
-	for i, item := range items {
-		if (1<<i)&mask != 0 {
-			result = append(result, item)
-		}
-	}
-	return
-}
-
-func MakeCombinations(items []int) [][]int {
-	length := 1 << len(items)
-	combinations := make([][]int, 0)
-	for i := 1; i < length; i++ {
-		combination := Mask(items, i)
-		combinations = append(combinations, combination)
-	}
-	return combinations
-}
-
-func MakeCombinationsRecursive(items []int, depth int, last int) (combinations [][]int) {
+func CountCombinations(items []int, depth int, last int) (combinations int) {
 	if depth >= len(items) {
 		return
 	}
@@ -92,48 +65,17 @@ func MakeCombinationsRecursive(items []int, depth int, last int) (combinations [
 		return
 	}
 
-	other := MakeCombinationsRecursive(items, depth+1, last)
-	combinations = append(combinations, other...)
+	combinations += CountCombinations(items, depth+1, last)
 
 	skip := make([]int, depth)
 	copy(skip, items[:depth])
 	skip = append(skip, items[depth+1:]...)
-	other = MakeCombinationsRecursive(skip, depth, last)
-	combinations = append(combinations, other...)
+	combinations += CountCombinations(skip, depth, last)
 
 	if items[depth] == last {
-		combinations = append(combinations, items[:depth+1])
+		combinations++
 	}
 	return
-}
-
-func ShortestPath(sorted []int) []int {
-	i := 1
-	for i < len(sorted)-1 {
-		difference := sorted[i+1] - sorted[i-1]
-		if difference <= 3 {
-			sorted = append(sorted[:i], sorted[i+1:]...)
-		} else {
-			i++
-		}
-	}
-	return sorted
-}
-
-// (0), 1, 4, 5, 7, (10)
-// (0), 1, 4, 7, (10)
-func CountCombinations(sorted []int) (count int) {
-	shortestPath := ShortestPath(sorted)
-	from := len(shortestPath)
-	to := len(sorted)
-	for i := from; i < to; i++ {
-		count += Combinations(to, i)
-	}
-	return
-}
-
-func MultiplyEnds(items []int) int {
-	return items[0] * items[len(items)-1]
 }
 
 func main() {
@@ -147,7 +89,6 @@ func main() {
 
 	input := strings.Trim(string(content), "\n")
 	items := Sort(Parse(input))
-	combinations := MakeCombinationsRecursive(items, 0, items[len(items)-1])
-	result := len(combinations)
+	result := CountCombinations(items, 0, items[len(items)-1])
 	fmt.Println(result)
 }
--- day10/day10_test.go
+++ day10/day10_test.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"math"
 	"reflect"
 	"strconv"
 	"testing"
@@ -31,7 +29,7 @@ func TestFindJoltDifferences(t *testing.T) {
 	}
 }
 
-func TestCountCombinations(t *testing.T) {
+func TestPart2(t *testing.T) {
 	tests := []struct {
 		input string
 		want  int
@@ -52,33 +50,35 @@ func TestCountCombinations(t *testing.T) {
 	for i, test := range tests {
 		t.Run(strconv.Itoa(i), func(t *testing.T) {
 			items := Sort(Parse(test.input))
-			got := MakeCombinationsRecursive(items, 0, items[len(items)-1])
-			assertIntEqual(t, len(got), test.want)
+			got := CountCombinations(items, 0, items[len(items)-1])
+			assertIntEqual(t, got, test.want)
 		})
 	}
 }
 
-func TestShortestPath(t *testing.T) {
-	input := `1
-4
-5
-7`
-	items := Sort(Parse(input))
-	got := ShortestPath(items)
-	want := []int{1, 4, 7}
-	assertIntSliceEqual(t, got, want)
+func TestCountCombinations(t *testing.T) {
+	tests := []struct {
+		input []int
+		want  int
+	}{
+		{[]int{1, 2, 3, 4, 5, 6}, 24},
+		{[]int{1, 2, 3, 4, 5}, 13},
+		{[]int{1, 2, 3, 4}, 7},
+		{[]int{1, 2, 3}, 4},
+	}
+	for i, test := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			got := CountCombinations(test.input, 0, test.input[len(test.input)-1])
+			assertIntEqual(t, got, test.want)
+		})
+	}
 }
 
-func TestShortestPath2(t *testing.T) {
-	input := "28\n33\n18\n42\n31\n14\n46\n20\n48\n47\n24\n23\n49\n45\n19\n38\n39\n11\n1\n32\n25\n35\n8\n17\n7\n9\n4\n2\n34\n10\n3"
-	items := Sort(Parse(input))
-	got := len(ShortestPath(items))
-	long := math.Pow(2, float64(len(items)))
-	short := math.Pow(2, float64(got))
-	combinations := long - short
-	fmt.Printf("combinations: %f", combinations)
-	want := 19208
-	assertIntEqual(t, got, want)
+func BenchmarkCountCombinations(b *testing.B) {
+	input := []int{1, 2, 3, 4, 5, 6}
+	for i := 0; i < b.N; i++ {
+		CountCombinations(input, 0, input[len(input)-1])
+	}
 }
 
 func TestSort(t *testing.T) {
@@ -95,92 +95,6 @@ func TestSort(t *testing.T) {
 		want := []int{3, 2, 1}
 		assertIntSliceEqual(t, input, want)
 	})
-}
-
-func TestFactorial(t *testing.T) {
-	got := Factorial(4)
-	want := 24
-	assertIntEqual(t, got, want)
-}
-
-func TestMask(t *testing.T) {
-	input := []int{1, 2, 3}
-	tests := []struct {
-		mask int
-		want []int
-	}{
-		{1, []int{1}},
-		{2, []int{2}},
-		{3, []int{1, 2}},
-		{4, []int{3}},
-		{5, []int{1, 3}},
-		{6, []int{2, 3}},
-		{7, []int{1, 2, 3}},
-	}
-	for _, test := range tests {
-		t.Run(strconv.Itoa(test.mask), func(t *testing.T) {
-			got := Mask(input, test.mask)
-			assertIntSliceEqual(t, got, test.want)
-		})
-	}
-}
-
-func TestCombinations(t *testing.T) {
-	tests := []struct {
-		n    int
-		r    int
-		want int
-	}{
-		{4, 4, 1},
-		{4, 3, 4},
-		{4, 2, 6},
-		{4, 1, 4},
-	}
-	for _, test := range tests {
-		name := fmt.Sprintf("%v", test)
-		t.Run(name, func(t *testing.T) {
-			got := Combinations(test.n, test.r)
-			assertIntEqual(t, got, test.want)
-		})
-	}
-}
-
-func TestMakeCombination(t *testing.T) {
-	tests := []struct {
-		input []int
-		want  int
-	}{
-		{[]int{1, 2, 3, 4, 5, 6}, 63},
-		{[]int{1, 2, 3, 4, 5}, 31},
-		{[]int{1, 2, 3, 4}, 15},
-		{[]int{1, 2, 3}, 7},
-	}
-	for i, test := range tests {
-		t.Run(strconv.Itoa(i), func(t *testing.T) {
-			got := MakeCombinations(test.input)
-			assertIntEqual(t, len(got), test.want)
-		})
-	}
-}
-
-func TestMakeCombinationRecursive(t *testing.T) {
-	t.SkipNow()
-	tests := []struct {
-		input []int
-		want  int
-	}{
-		{[]int{1, 2, 3, 4, 5, 6}, 63},
-		{[]int{1, 2, 3, 4, 5}, 31},
-		{[]int{1, 2, 3, 4}, 15},
-		{[]int{1, 2, 3}, 7},
-	}
-	for i, test := range tests {
-		t.Run(strconv.Itoa(i), func(t *testing.T) {
-			got := MakeCombinationsRecursive(test.input, 0, test.input[len(test.input)-1])
-			fmt.Println(got)
-			assertIntEqual(t, len(got), test.want)
-		})
-	}
 }
 
 func assertIntEqual(t *testing.T, got, want int) {
