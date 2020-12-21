b62831b33117f71f9a28cf410d2aed9f6a89e4fe
Paul Heasley <paul.heasley@acloud.guru>
2020-12-22 08:25:32 +1100

    WIP day11

--- day11/day11.go
+++ day11/day11.go
@@ -5,26 +5,81 @@ import (
 	"strings"
 )
 
-func CountOccupiedSeats(input string) int {
-	width := strings.Index(input, "\n")
-	seats := strings.ReplaceAll(input, "\n", "")
+func MarkAdjacent(i int, width int, adjacent map[int]int) {
+	adjacent[i+width]++
+	adjacent[i-width]++
+	// If not at the end of row
+	if (i+1)%width != 0 {
+		adjacent[i+1]++
+		adjacent[i+width+1]++
+		adjacent[i-width+1]++
+	}
+	// If not at the start of row
+	if i%width != 0 {
+		adjacent[i-1]++
+		adjacent[i+width-1]++
+		adjacent[i-width-1]++
+	}
+}
+
+func CountAdjacent(i int, width int, adjacent map[int]int) (count int) {
+	count += adjacent[i+width]
+	count += adjacent[i-width]
+	// If not at the end of row
+	if (i+1)%width != 0 {
+		count += adjacent[i+1]
+		count += adjacent[i+width+1]
+		count += adjacent[i-width+1]
+	}
+	// If not at the start of row
+	if i%width != 0 {
+		count += adjacent[i-1]
+		count += adjacent[i+width-1]
+		count += adjacent[i-width-1]
+	}
+	return
+}
+
+func replaceAtIndex(in string, r rune, i int) string {
+	out := []rune(in)
+	out[i] = r
+	return string(out)
+}
+
+func Evaluate(seats string, width int) string {
 	adjacent := make(map[int]int, 0)
 	for i, seat := range seats {
 		if seat == '#' {
-			adjacent[i+width]++
-			adjacent[i-width]++
-			if (i+1)%width == 0 {
-				adjacent[i+1]++
-				adjacent[i+width+1]++
-				adjacent[i-width+1]++
+			MarkAdjacent(i, width, adjacent)
+		}
+	}
+	for i, seat := range seats {
+		adjacentCount := CountAdjacent(i, width, adjacent)
+		switch seat {
+		case 'L':
+			if adjacentCount == 0 {
+				seats = replaceAtIndex(seats, '#', i)
 			}
-			if (i+1)%width == 1 {
-				adjacent[i-1]++
-				adjacent[i+width-1]++
-				adjacent[i-width-1]++
+		case '#':
+			if adjacentCount >= 4 {
+				seats = replaceAtIndex(seats, 'L', i)
 			}
 		}
 	}
-	fmt.Println(adjacent)
-	return 0
+	return seats
+}
+
+func CountOccupiedSeats(input string) int {
+	width := strings.Index(input, "\n")
+	seats := strings.ReplaceAll(input, "\n", "")
+	settled := Evaluate(seats, width)
+	for {
+		next := Evaluate(settled, width)
+		if next == settled {
+			break
+		}
+		settled = next
+		fmt.Println(settled)
+	}
+	return strings.Count(settled, "#")
 }
--- day11/day11_test.go
+++ day11/day11_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
 
 func TestCountOccupiedSeats(t *testing.T) {
 	input := `L.LL.LL.LL
@@ -18,9 +22,71 @@ L.LLLLL.LL`
 	assertIntEqual(t, got, want)
 }
 
+func TestEvaluate(t *testing.T) {
+	t.Run("should occupy every seat given no seats occupied", func(t *testing.T) {
+		input := strings.ReplaceAll(`L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL`, "\n", "")
+		got := Evaluate(input, 10)
+		want := strings.ReplaceAll(`#.##.##.##
+#######.##
+#.#.#..#..
+####.##.##
+#.##.##.##
+#.#####.##
+..#.#.....
+##########
+#.######.#
+#.#####.##`, "\n", "")
+		assertStringEqual(t, got, want)
+	})
+}
+
+func TestMarkAdjacent(t *testing.T) {
+	t.Run("should not mark seats before start of row", func(t *testing.T) {
+		got := make(map[int]int)
+		MarkAdjacent(10, 10, got)
+		want := map[int]int{0: 1, 1: 1, 11: 1, 20: 1, 21: 1}
+		assertMapEqual(t, got, want)
+	})
+	t.Run("should not mark seats after end of row", func(t *testing.T) {
+		got := make(map[int]int)
+		MarkAdjacent(19, 10, got)
+		want := map[int]int{8: 1, 9: 1, 18: 1, 28: 1, 29: 1}
+		assertMapEqual(t, got, want)
+	})
+	t.Run("should update existing values", func(t *testing.T) {
+		got := map[int]int{1: 2}
+		MarkAdjacent(0, 10, got)
+		want := 3
+		assertIntEqual(t, got[1], want)
+	})
+}
+
 func assertIntEqual(t *testing.T, got, want int) {
 	t.Helper()
 	if got != want {
 		t.Errorf("Wanted %d, got %d", want, got)
+	}
+}
+
+func assertStringEqual(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("Wanted %q, got %q", want, got)
+	}
+}
+
+func assertMapEqual(t *testing.T, got, want map[int]int) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Wanted %v, got %v", want, got)
 	}
 }
