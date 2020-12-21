bd1c5cb81352454dd676b64860e1d3de6ad00fb7
Paul Heasley <paul.heasley@acloud.guru>
2020-12-21 12:56:12 +1100

    Start day11

---
+++ day11/day11_test.go
@@ -0,0 +1,26 @@
+package main
+
+import "testing"
+
+func TestCountOccupiedSeats(t *testing.T) {
+	input := `L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL`
+	got := CountOccupiedSeats(input)
+	want := 37
+	assertIntEqual(t, got, want)
+}
+
+func assertIntEqual(t *testing.T, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("Wanted %d, got %d", want, got)
+	}
+}
